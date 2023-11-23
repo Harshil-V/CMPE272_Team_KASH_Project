70d50280468a5a67e1c5004373a6c187a0353d65
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2023-11-23 10:30:55 -0800

    added styling to postion login component.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.jsx'
 // import './App.css'
-// import './index.css'
+import './index.css'
 // import 'bootstrap/dist/css/bootstrap.css';
 
 import { Amplify, Auth } from 'aws-amplify';
